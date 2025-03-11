5b11da53a9c9e77045e058bafc35148c771a3d0a
dinihz <lucas87diniz@gmail.com>
2025-03-11 18:53:50 -0300

    add neo-tree and presence

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,33 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Presence Plugin
+  {
+    'andweeb/presence.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('presence').setup {
+        auto_update = true,
+        neovim_image_text = 'The One True Text Editor',
+        main_image = 'neovim',
+        client_id = '793271441293967371',
+        debounce_timeout = 10,
+        enable_line_number = false,
+        blacklist = {},
+        buttons = true,
+        file_assets = {},
+        show_time = false,
+
+        -- Rich Presence text options
+        editing_text = 'Editing file',
+        file_explorer_text = 'Browsing file',
+        git_commit_text = 'Committing changes',
+        plugin_manager_text = 'Managing plugins',
+        reading_text = 'Reading file',
+        workspace_text = 'Working on project',
+        line_number_text = 'Line %s out of %s',
+      }
+    end,
+  },
+}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,22 +2,81 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    keys = {
+      {
+        '<leader>e',
+        ':Neotree reveal<CR>',
+        desc = 'NeoTree reveal',
+        silent = true,
+      },
+    },
+    opts = {
+      sources = { 'filesystem', 'buffers', 'git_status' },
       window = {
+        position = 'right',
+        width = 30,
         mappings = {
-          ['\\'] = 'close_window',
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['<space>'] = 'none',
+          ['Y'] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg('+', path, 'c')
+            end,
+            desc = 'Copy Path to Clipboard',
+          },
+          ['O'] = {
+            function(state)
+              require('lazy.util').open(state.tree:get_node().path, { system = true })
+            end,
+            desc = 'Open with System Application',
+          },
+          ['P'] = { 'toggle_preview', config = { use_float = false } },
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
+        },
+        git_status = {
+          symbols = {
+            unstaged = '󰄱',
+            staged = '󰱒',
+          },
+        },
+      },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = {
+            '.git',
+            '.DS_Store',
+          },
+          always_show = {
+            '.env',
+          },
+        },
+        window = {
+          mappings = {
+            ['<leader>e'] = 'close_window',
+          },
         },
       },
     },
